4c8911f58710815f063e830832d26ae838bc1978
Cierra-Runis <2864283875@qq.com>
2023-01-03 14:24:38 +0800

    修改

--- .release_tool/main.py
+++ .release_tool/main.py
@@ -4,10 +4,52 @@
 
 import os
 
-if __name__ == '__main__':
-    os.system('cls')
-    print('-- main.py --')
 
+def input_tool(
+    first_message: str,
+    rule: str,
+    error_message: str,
+    rule_function: any,
+) -> str:
+    '''
+    根据 rule_function 获取合法的值
+    '''
+    # 提醒
+    print(f'> {first_message} {rule}: ', end='')
+    # 第一次输入
+    input_str = input()
+    # 当 rule_function(input_str) 返回 false
+    # 即不合法时
+    while not rule_function(input_str):
+        # 重新提醒并输入
+        print(f'> {error_message}')
+        print(f'> {first_message} {rule}: ', end='')
+        input_str = input()
+    # 直至输入合法
+    return input_str
+
+
+def build_module() -> None:
+    '''
+    构建模块
+    '''
+    print('> 清除旧构建')
+    os.system('hexo clean')
+
+    print('> 获取最新 bangumi 资讯')
+    os.system('hexo bangumi -u')
+
+    print('> 构建中')
+    os.system('hexo generate')
+
+    print('> 开启服务中')
+    os.system('hexo server')
+
+
+def release_module() -> None:
+    '''
+    发布模块
+    '''
     print('> 清除旧构建')
     os.system('hexo clean')
 
@@ -27,3 +69,20 @@ if __name__ == '__main__':
     os.system(f'git commit -m "{release_str}"')
     os.system('git push')
     print(f'> 已发布携带信息为 "{release_str}" 的仓库, 请检查发布情况')
+
+
+if __name__ == '__main__':
+    os.system('cls')
+    print('-- main.py --')
+    input_str = ''
+    input_str = input_tool(
+        first_message='> 是否进入发布模式，反之进入构建模式',
+        rule='(y/n)',
+        error_message='请只输入 y 或 n',
+        rule_function=lambda input_str: input_str == 'y' or input_str == 'n',
+    )
+
+    if input_str == 'y':
+        release_module()
+    else:
+        build_module()
